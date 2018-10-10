521c8deae8c30dc0b2806fa0d8ecf6ba2b4cba4b
Mark David Avery <mark@webark.cc>
2018-10-09 22:03:21 -0700

    feat: base styles package

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 'use strict';
 
+const path = require('path');
+
+const Funnel = require('broccoli-funnel');
+const Merge = require('broccoli-merge-trees');
+
 module.exports = {
+  preprocessTree(type, tree) {
+    if (type === 'css') {
+      const mySass = new Funnel(path.join(this.root, 'styles'))
+      tree = Merge([tree, mySass]);
+    }
+
+    return tree;
+  },
+
   name: require('./package').name
 };
